81a16d1c8ab2368c2eff02b1c2dd60b72264383e
Hai Nguyen <haibeo@gmail.com>
2026-03-29 17:42:52 +0700

    test: Add comprehensive Queue tests and improve coverage
    - Expand Queue tests with Job macro tests and API tests
    - Add CSRF and SecureHeaders plugin tests
    - Fix Router.DSL tests for 100% coverage
    - Fix FileStreamer test expectations
    - 301 tests running, coverage improving steadily

--- apps/hibana/test/queue_test.exs
+++ apps/hibana/test/queue_test.exs
@@ -1,21 +1,49 @@
 defmodule Hibana.QueueTest do
   use ExUnit.Case, async: false
 
-  describe "Job macro" do
-    defmodule TestJob do
-      use Hibana.Queue.Job
+  alias Hibana.Queue
 
-      def perform(data) do
-        {:ok, data}
-      end
+  defmodule TestJob do
+    use Hibana.Queue.Job
+
+    def perform(data) do
+      {:ok, data}
     end
+  end
 
+  describe "Job macro" do
     test "creates enqueue function" do
       assert function_exported?(TestJob, :enqueue, 2)
     end
 
     test "creates enqueue_at function" do
       assert function_exported?(TestJob, :enqueue_at, 3)
+    end
+
+    test "perform function returns data" do
+      assert TestJob.perform(%{test: "data"}) == {:ok, %{test: "data"}}
+    end
+  end
+
+  describe "Queue API" do
+    test "start_link with default name" do
+      {:ok, pid} = Queue.start_link(name: :test_default_queue)
+      assert Process.alive?(pid)
+      GenServer.stop(pid)
+    end
+
+    test "enqueue returns job id" do
+      {:ok, pid} = Queue.start_link(name: :test_api_queue)
+
+      defmodule SimpleWorker do
+        def perform(_args), do: :ok
+      end
+
+      result = Queue.enqueue(SimpleWorker, %{}, [], :test_api_queue)
+      assert {:ok, job_id} = result
+      assert is_binary(job_id)
+
+      GenServer.stop(pid)
     end
   end
 end
